0c57a8f23ad714a27041695bb81050bafd8dc0f3
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2023-09-21 22:20:19 +0100

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -34,7 +34,7 @@ router.post('/question1-eligibility', function(request, response) {
 router.post('/question2-case-hearing', function(request, response) {
 
     var courtType = request.session.data['courtType']
-    if (courtType == "Crown court (excluding Central Criminal Court)"){
+    if (courtType == "Crown Court (excluding Central Criminal Court)"){
         response.redirect("provider/form/task-list?question3-case=complete")
     } else {
         response.redirect("/provider/form/question2-case-hearing-extra")
@@ -78,13 +78,72 @@ router.post('/question3-quote-add-summary', function(request, response) {
 })
 
 //Alternative quotes question (Section3)
-router.post('/question3-alt-quotes', function(request, response) {
+router.post('/question3-alt-quotes-q', function(request, response) {
 
-    var altQuote = request.session.data['alt-quotes-q']
-    if (altQuote == "No"){
-        response.redirect("/provider/form/task-list?question3-alt-quotes=complete")
-    } else {
-        response.redirect("/provider/form/question3-alt-quote-details?addAltQuote=1")
+    var altQuotesQ = request.session.data['altQuotesQ']
+    if (altQuotesQ == "No"){
+        response.redirect("/provider/form/task-list?question3-alt-quote=complete")
+    } 
+    if (altQuotesQ == "Yes"){
+        response.redirect("/provider/form/question3-alt-quote-details?addAlt=yes&altShow=1&addAltQuote=1")
+    }
+})
+
+//Alternative quotes travel question (Section3)
+router.post('/question3-alt-travel-q1', function(request, response) {
+
+    var altTravelQ1 = request.session.data['altTravelQ1']
+    if (altTravelQ1 == "No"){
+        response.redirect("/provider/form/question3-alt-quote-summary")
+    } 
+    if (altTravelQ1 == "Yes"){
+        response.redirect("/provider/form/question3-alt-quote-travel")
+    }
+})
+
+router.post('/question3-alt-travel-q2', function(request, response) {
+
+    var altTravelQ2 = request.session.data['altTravelQ2']
+    if (altTravelQ2 == "No"){
+        response.redirect("/provider/form/question3-alt-quote-summary")
+    } 
+    if (altTravelQ2 == "Yes"){
+        response.redirect("/provider/form/question3-alt-quote-travel")
+    }
+})
+
+router.post('/question3-alt-travel-q3', function(request, response) {
+
+    var altTravelQ3 = request.session.data['altTravelQ3']
+    if (altTravelQ3 == "No"){
+        response.redirect("/provider/form/question3-alt-quote-summary")
+    } 
+    if (altTravelQ3 == "Yes"){
+        response.redirect("/provider/form/question3-alt-quote-travel")
+    }
+})
+
+//Alternative quotes add 2nd quote (Section3)
+router.post('/question3-alt-quote-summary2', function(request, response) {
+
+    var quoteAddAltOther2 = request.session.data['altAddQuote1']
+    if (quoteAddAltOther2 == "No"){
+        response.redirect("/provider/form/task-list?question3-alt-quote=complete")
+    } 
+    if (quoteAddAltOther2 == "Yes"){
+        response.redirect("/provider/form/question3-alt-quote-details?addAlt=yes&altShow=2&addAltQuote=2")
+    }
+})
+
+//Alternative quotes add 3rd quote (Section3)
+router.post('/question3-alt-quote-summary3', function(request, response) {
+
+    var quoteAddAltOther3 = request.session.data['altAddQuote2']
+    if (quoteAddAltOther3 == "No"){
+        response.redirect("/provider/form/task-list?question3-alt-quote=complete")
+    } 
+    if (quoteAddAltOther3 == "Yes"){
+        response.redirect("/provider/form/question3-alt-quote-details?addAlt=yes&altShow=3&addAltQuote=3")
     }
 })
 
@@ -99,7 +158,3 @@ router.post('/question3-add-info', function(request, response) {
         response.redirect("/provider/form/task-list?question3-add-info=complete")
     }
 })
-
-
-
-
