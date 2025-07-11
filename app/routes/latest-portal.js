58f524bd240a31c614fdf19e236b5a4af9d12965
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-07-11 12:56:58 +0100

    Payment: Change of flow for when there is an existing claim or payment as should not show editible fields

--- app/routes/latest-portal.js
+++ app/routes/latest-portal.js
@@ -3,6 +3,30 @@ const router = govukPrototypeKit.requests.setupRouter('/latest-portal')
 
 const version = 'latest-portal'
 
+//Payment type?
+router.post('/question-payment-type', function(request, response) {
+
+    var paymentType = request.session.data['paymentType']
+
+    if (paymentType == "Non-standard Magistrates'"){
+        response.redirect('/' + version + "/payments/add/claim-details")
+    } 
+
+    if (paymentType == "Supplemental Non-standard Magistrates'"){
+        response.redirect('/' + version + "/payments/add/existing-claim-question")
+    } 
+
+    if (paymentType == "Amendment to Non-standard Magistrates'"){
+        response.redirect('/' + version + "/payments/add/existing-claim-question")
+    } 
+     if (paymentType == "Assigned counsel"){
+        response.redirect('/' + version + "/payments/add/existing-claim-question")
+    } 
+    if (paymentType == "Amendment to assigned counsel"){
+        response.redirect('/' + version + "/payments/add/existing-claim-question")
+    } 
+  })
+  
 //Payments - Existing claim?
 router.post('/question-existing-claim-ref', function(request, response) {
 
@@ -11,7 +35,7 @@ router.post('/question-existing-claim-ref', function(request, response) {
     if (existingRef == "Yes"){
         response.redirect('/' + version + "/payments/add/laa-reference")
     } 
-    else {
+    if (existingRef == "No") {
         response.redirect('/' + version + "/payments/add/claim-details")
     } 
   })
@@ -22,9 +46,10 @@ router.post('/question-existing-counsel-ref', function(request, response) {
     var existingRefCounsel = request.session.data['existingRefCounsel']
 
     if (existingRefCounsel == "Yes"){
-        response.redirect('/' + version + "/payments/add/claim-details")
+        response.redirect('/' + version + "/payments/add/claim-details?laa-ref=LAA-5a7d3c&dateReceived=24 July 2025&officeAccount=1A33E4&ufn=100323/234&counselOfficeAccount=4A33E9&defendantLastName=Blackbeard")
     } 
-    else {
-        response.redirect('/' + version + "/payments/add/laa-reference")
+    if (existingRefCounsel == "No") {
+        response.redirect('/' + version + "/payments/add/existing-claim-question?laa-ref=&dateReceived=&officeAccount=&ufn=&counselOfficeAccount=&defendantLastName=")
     } 
   })
+
