be573d280030b04f9f5eb5a74df7816f38f0f65c
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-07-16 18:14:38 +0100

    Changes to flow after talks with Matt - fee cat/ appeal/ supplemental

--- app/routes/latest-portal.js
+++ app/routes/latest-portal.js
@@ -13,14 +13,14 @@ router.post('/question-payment-type', function(request, response) {
     } 
 
     if (paymentType == "Supplemental Non-standard Magistrates'"){
-        response.redirect('/' + version + "/payments/add/existing-claim-question")
+        response.redirect('/' + version + "/payments/add/laa-reference")
     } 
 
     if (paymentType == "Amendment to Non-standard Magistrates'"){
-        response.redirect('/' + version + "/payments/add/existing-claim-question")
+        response.redirect('/' + version + "/payments/add/laa-reference")
     } 
      if (paymentType == "Assigned counsel"){
-        response.redirect('/' + version + "/payments/add/existing-claim-question")
+        response.redirect('/' + version + "/payments/add/laa-reference")
     } 
     if (paymentType == "Amendment to assigned counsel"){
         response.redirect('/' + version + "/payments/add/existing-claim-question")
