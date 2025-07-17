e1bd10ff607350120c42a63f4e64d04129e75c3f
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-07-17 16:08:37 +0100

    Minor amends to dispaly of claims and amendment to AC flow

--- app/routes/latest-portal.js
+++ app/routes/latest-portal.js
@@ -23,33 +23,6 @@ router.post('/question-payment-type', function(request, response) {
         response.redirect('/' + version + "/payments/add/laa-reference")
     } 
     if (paymentType == "Amendment to assigned counsel"){
-        response.redirect('/' + version + "/payments/add/existing-claim-question")
-    } 
-  })
-  
-//Payments - Existing claim?
-router.post('/question-existing-claim-ref', function(request, response) {
-
-    var existingRef = request.session.data['existingRef']
-
-    if (existingRef == "Yes"){
         response.redirect('/' + version + "/payments/add/laa-reference")
     } 
-    if (existingRef == "No") {
-        response.redirect('/' + version + "/payments/add/claim-details")
-    } 
   })
-
-//Payments - Existing claim - counsel assigned?
-router.post('/question-existing-counsel-ref', function(request, response) {
-
-    var existingRefCounsel = request.session.data['existingRefCounsel']
-
-    if (existingRefCounsel == "Yes"){
-        response.redirect('/' + version + "/payments/add/claim-details?laa-ref=LAA-5a7d3c&dateReceived=24 July 2025&officeAccount=1A33E4&ufn=100323/234&counselOfficeAccount=4A33E9&defendantLastName=Blackbeard")
-    } 
-    if (existingRefCounsel == "No") {
-        response.redirect('/' + version + "/payments/add/existing-claim-question?laa-ref=&dateReceived=&officeAccount=&ufn=&counselOfficeAccount=&defendantLastName=")
-    } 
-  })
-
