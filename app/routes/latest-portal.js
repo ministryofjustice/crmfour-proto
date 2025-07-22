f75085cedbd165ddd1e8d29699c63f2be95a67bf
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-07-22 16:40:27 +0100

    Content changes

--- app/routes/latest-portal.js
+++ app/routes/latest-portal.js
@@ -8,11 +8,11 @@ router.post('/question-payment-type', function(request, response) {
 
     var paymentType = request.session.data['paymentType']
 
-    if (paymentType == "Non-standard Magistrates'" ){
+    if (paymentType == "Non-standard magistrates'" ){
         response.redirect('/' + version + "/payments/add/claim-details?claimedProfit=&claimedTravel=&claimedWaiting=&claimedDisbursement=&allowedProfit=&allowedTravel=&allowedWaiting=&allowedDisbursement=")
     } 
 
-    if (paymentType == "Non-standard Magistrates' supplemental"){
+    if (paymentType == "Non-standard magistrates' - supplemental"){
         response.redirect('/' + version + "/payments/add/laa-reference?claimedProfit=342.00&claimedTravel=0.00&claimedWaiting=60.00&claimedDisbursement=890.00&allowedProfit=332.00&allowedTravel=50.00&allowedWaiting=0.00&allowedDisbursement=890.00")
     } 
 
